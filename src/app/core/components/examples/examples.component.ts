9ec30d8760690ff540616c762f1e9f4cb6d8245b
Franz Bendezu <franzant20@gmail.com>
2023-03-15 19:02:56 -0500

    refactor: moviendo ejemplos a un componente

---
+++ src/app/core/components/examples/examples.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-examples',
+  templateUrl: './examples.component.html',
+  styleUrls: ['./examples.component.css']
+})
+export class ExamplesComponent {
+  title = 'project 123';
+
+  currentDate = new Date().toISOString();
+
+  price = 12.5;
+
+  username = 'Juan';
+
+  isLoggedIn = false;
+
+  fullName = 'Pedro M.';
+
+  constructor(public appService: DataService) {
+    console.log(appService.getData());
+  }
+
+  onClick() {
+    this.fullName = this.fullName.toUpperCase();
+  }
+
+  onChange($event: any) {
+    console.log($event.target.value);
+  }
+
+  onInput($event: any) {
+    console.log($event.target.value);
+  }
+
+  isShowLayout = true;
+  hideLayout() {
+    this.isShowLayout = false;
+  }
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './components/layout/layout.component';
 import { DataService } from './services/data.service';
 import { ExamplesComponent } from './components/examples/examples.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [LayoutComponent, ExamplesComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   providers: [DataService],
-  exports: [LayoutComponent],
+  exports: [LayoutComponent, ExamplesComponent],
 })
 export class CoreModule {}
