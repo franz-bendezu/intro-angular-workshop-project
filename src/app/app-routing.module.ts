93258355d9bbf48725da58ac823e6e128dc8dc1c
Franz Bendezu <franzant20@gmail.com>
2023-03-15 19:32:45 -0500

    feat: comfiguración de examples como pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExamplesComponent } from './core/components/examples/examples.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  component: ExamplesComponent,
+  path: ''
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
